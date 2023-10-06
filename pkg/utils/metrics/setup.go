a6d07a596d446e1d9527edc21f8a1c01b60ae63e
Kirill Sushkov (teeverr) <kirill.sushkov@swisscom.com>
2023-10-06 18:19:51 +0200

    add reconcilation metrics, draft

--- pkg/utils/metrics/setup.go
+++ pkg/utils/metrics/setup.go
@@ -10,11 +10,17 @@ var (
 		Name: "aws_api_calls_total",
 		Help: "Number of API calls to the AWS API",
 	}, []string{"service", "operation", "api_version"})
+	MetricAWSAPIRecCalls = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "aws_api_reconciliation_calls",
+		Help: "Number of calls of the AWS API produced by controller to provide reconciliation per managed resource and operation type",
+	}, []string{"service", "resource_group", "resource_name", "controller_operation_type"})
 )
 
 // SetupMetrics will register the known Prometheus metrics with controller-runtime's metrics registry
-func SetupMetrics() error {
-	return k8smetrics.Registry.Register(metricAWSAPICalls)
+func SetupMetrics() {
+	k8smetrics.Registry.MustRegister(
+		metricAWSAPICalls,
+		MetricAWSAPIRecCalls)
 }
 
 // IncAWSAPICall will increment the aws_api_calls_total metric for the specified service, operation, and apiVersion tuple
